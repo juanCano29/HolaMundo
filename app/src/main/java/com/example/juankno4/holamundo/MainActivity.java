045b6ced0adad1db9393e29e569ef81a1ece2a72
juanCano29 <juankno4@gmail.com>
2019-01-10 21:39:08 -0600

    Initial commit

---
+++ app/src/main/java/com/example/juankno4/holamundo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.juankno4.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button f = findViewById(R.id.miboton);
+        f.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"HolaMundo",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
